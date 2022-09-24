3fcd8e417fb51adc618ac5d8a6739292e9602a0c
“raphael” <plassa_r@etna-alternance.net>
2022-09-24 06:02:57 -0400

    commit

--- index.ts
+++ index.ts
@@ -4,22 +4,23 @@ const wordToGuess: string = lib.getRandomWord();
 console.log(wordToGuess);
 for (let a : number = 0; a < 6; a += 1) {
   const playerInput : string = lib.getInput('Type a word');
-  if (playerInput !== playerInput.toUpperCase() && playerInput.length !== 5) {
+  if (playerInput !== playerInput.toUpperCase() || playerInput.length !== 5) {
     console.log('Please, type a correct answer');
     a -= 1;
   }
   if (playerInput === wordToGuess) {
     console.log('Congratulations, you guessed the word.');
     break;
-  } else {
+  } if (playerInput !== wordToGuess) {
     const inputTab : any[] = [];
-    inputTab.push(playerInput);
     for (let b : number; b < 6; b += 1) {
       if (wordToGuess[b] === playerInput[b]) {
-        inputTab[b] = `\x1b[${b}[20m`;
+        inputTab.push(`\x1b[33m${b} \x1b[10m`);
       }
       if (inputTab[b].includes(wordToGuess[b]) === true) {
-        inputTab[b] = `\x1b[${b}[10m`;
+        inputTab.push(`\x1b[33m${b} \x1b[14m`);
+      } else {
+        inputTab.push(b);
       }
     }
     console.log(inputTab);
